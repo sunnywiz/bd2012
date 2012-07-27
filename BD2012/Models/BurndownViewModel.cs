44d00246dc5510390ef2ff87052fb13a7cffdf17
Sunny Gulati <sunnywiz@gmail.com>
2012-07-27 18:25:11 -0400

    WIP - builds

--- BD2012/Models/BurndownViewModel.cs
+++ BD2012/Models/BurndownViewModel.cs
@@ -1,20 +1,36 @@
 ﻿
 
 using System.Collections.Generic;
+using System.Linq;
+using BD2012.Code;
 
 namespace BD2012.Models
 {
     public class BurndownViewModel
     {
+        public List<RowViewModel> Rows { get; set; }
 
-        public List<BurndownRowModel> Rows { get; set; }
+        public BurndownViewModel CopyFrom(Burndown bd)
+        {
+            this.Rows = bd.Rows.Select(x => new RowViewModel().CopyFrom(x)).ToList();
+            return this; 
+        }
     }
 
-    public class BurndownRowModel
+    public class RowViewModel
     {
         public int LineItemId { get; set;  }
         public string Name { get; set;  }
         public int PixelIndent { get; set;  }
+
+        public RowViewModel CopyFrom(LineItem item)
+        {
+            LineItemId = item.Id; 
+            Name = item.Name;
+            PixelIndent = LineItemId*10;
+            return this;
+        }
     }
+
 
 }
