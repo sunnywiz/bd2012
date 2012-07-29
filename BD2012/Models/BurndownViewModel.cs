a8e5445c79971b4d8860df7a7956ac6427d4a42e
Sunny Gulati <sunnywiz@gmail.com>
2012-07-29 00:09:44 -0400

    Redo viewmodels indent works

--- BD2012/Models/BurndownViewModel.cs
+++ BD2012/Models/BurndownViewModel.cs
@@ -10,25 +10,89 @@ namespace BD2012.Models
     {
         public List<RowViewModel> Rows { get; set; }
 
+        public List<RowViewModel> RootRows { get; set; }
+
+
         public BurndownViewModel CopyFrom(Burndown bd)
         {
-            this.Rows = bd.Rows.Select(x => new RowViewModel().CopyFrom(x)).ToList();
+            Rows = new List<RowViewModel>(); 
+            RootRows = new List<RowViewModel>(); 
+            var unassigned = new List<RowViewModel>();
+            var dictOrig = new Dictionary<int, LineItem>();
+            var dictRvm = new Dictionary<int, RowViewModel>(); 
+
+            foreach (var row in bd.Rows)
+            {
+                var rvm = new RowViewModel().CopyFrom(row);
+                
+                Rows.Add(rvm);
+                if (row.Parent == null) { 
+                    RootRows.Add(rvm);
+                    rvm.Hierarchy = rvm.LineItemId.ToString();
+                    rvm.PixelIndent = 0; 
+                } else { 
+                    unassigned.Add(rvm); 
+                }
+                dictRvm[row.Id] = rvm;
+                dictOrig[row.Id] = row; 
+            }
+
+            while (unassigned.Count > 0)
+            {
+                for (int i = 0; i < unassigned.Count; i++)
+                {
+                    var iggy = unassigned[i];
+                    var orig = dictOrig[iggy.LineItemId];
+                    var origParent = orig.Parent; 
+                    var iggyparent = dictRvm[origParent.Id];
+
+                    if (iggyparent.Hierarchy != null)
+                    {
+                        iggyparent.Children.Add(iggy);
+                        iggy.Parent = iggyparent;
+                        iggy.Hierarchy = iggyparent.Hierarchy + "/" + iggy.LineItemId;
+                        iggy.PixelIndent = iggyparent.PixelIndent + 20; 
+                        unassigned.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
             return this; 
         }
     }
 
     public class RowViewModel
     {
+        public RowViewModel()
+        {
+            Children = new List<RowViewModel>();
+        }
+
+        public string Hierarchy { get; set; } 
         public int LineItemId { get; set;  }
         public string Name { get; set;  }
         public int PixelIndent { get; set;  }
+
+        public decimal? Low { get; set; }
+        public decimal? High { get; set; }
+        public decimal? Left { get; set; }
+        public decimal? Actual { get; set; }
+
+        public List<RowViewModel> Children { get; set; }
+        public RowViewModel Parent { get; set; }
 
         public RowViewModel CopyFrom(LineItem item)
         {
             LineItemId = item.Id; 
             Name = item.Name;
-            PixelIndent = LineItemId*10;
             return this;
+        }
+
+        public RowViewModel CopyFrom(Snapshot snapshot)
+        {
+            Low = snapshot.Low;
+            High = snapshot.High;
+            return this;  
         }
     }
 
