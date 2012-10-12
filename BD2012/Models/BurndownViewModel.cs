7eaa111373153d1949a7557fb0f52f575ce3b040
Sunny Gulati <sunnywiz@gmail.com>
2012-10-12 14:32:24 -0400

    WIP - don't remember what - doesn't compile

---
+++ BD2012/Models/BurndownViewModel.cs
@@ -0,0 +1,182 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using bd2012.data;
+using System;
+
+namespace BD2012.Models
+{
+    public class BurndownViewModel
+    {
+        public BurndownViewModel()
+        {
+            AllRows = new List<RowViewModel>();
+            HierarchicalRows = new List<RowViewModel>();
+            ColumnNames = new List<string>();
+            ColumnEditability = new List<bool>(); 
+        }
+
+        public string Name { get; set; }
+        public List<RowViewModel> HierarchicalRows { get; private set; } 
+        public List<RowViewModel> AllRows { get; private set; }
+        public List<string> ColumnNames { get; private set; }
+        public List<bool> ColumnEditability { get; private set; } 
+
+        public BurndownViewModel CopyFrom(Project project)
+        {
+            Name = project.ProjectName;
+
+            List<LineItem> parentItems;
+            List<LineItem> childItems;
+            SplitIntoParentVsChild(project, out parentItems, out childItems);
+
+            Dictionary<int, RowViewModel> dict = new Dictionary<int, RowViewModel>();
+            ConvertParents(parentItems, dict);
+            ConvertChildren(childItems, dict);
+
+            AllRows = new List<RowViewModel>();
+            RecurseAppend(HierarchicalRows, AllRows);
+
+            MapData(project, dict);
+
+            UpdateEditability(AllRows); 
+
+            foreach (var row in HierarchicalRows) 
+                UpdateCalculations(row);
+
+            return this; 
+        }
+
+        private void UpdateEditability(List<RowViewModel> AllRows)
+        {
+            for (int i = 0; i < ColumnNames.Count; i++)
+            {
+                string colName = ColumnNames[i];
+                bool editable = ColumnEditability[i];
+
+                foreach (var row in AllRows)
+                {
+                    CellViewModel cvm = null;
+                    if (row.Cells.TryGetValue(colName, out cvm))
+                    {
+                        cvm.IsEditable = editable; 
+                    }
+                }
+            }
+        }
+
+        private void UpdateCalculations(RowViewModel parentRow)
+        {
+            if (parentRow.Children != null && parentRow.Children.Count > 0)
+            {
+                foreach (var row in parentRow.Children) UpdateCalculations(row);
+
+                // i have children, therefore i am calculated
+                foreach (var c in ColumnNames)
+                {
+                    decimal sum = 0m;
+                    foreach (var childRow in parentRow.Children)
+                    {
+                        CellViewModel cvm;
+                        if (childRow.Cells.TryGetValue(c, out cvm))
+                        {
+                            sum += cvm.Value;
+                        }
+                    }
+                    if (!parentRow.Cells.ContainsKey(c))
+                        parentRow.Cells[c] = new CellViewModel();
+                    parentRow.Cells[c].Value = sum;
+                    parentRow.Cells[c].IsEditable = false;
+                }
+            }
+        }
+
+        private void MapData(Project project, Dictionary<int, RowViewModel> dict)
+        {
+            ColumnNames.Clear();
+            ColumnEditability.Clear(); 
+            foreach (var dataCol in project.ColumnDefinitions)
+            {
+                ColumnNames.Add(dataCol.ColumnName);
+                ColumnEditability.Add(dataCol.IsImmediateEntry);
+            }
+
+            foreach (var d in project.Data.Where(x => x.Value.HasValue))
+            {
+                RowViewModel li;
+                if (dict.TryGetValue(d.LineItem.LineItemId, out li))
+                {
+                    if (li == null) continue;
+                    if (!li.Cells.ContainsKey(d.Column.ColumnName)) li.Cells[d.Column.ColumnName] = new CellViewModel(); 
+                    li.Cells[d.Column.ColumnName].Value = d.Value.Value;
+                }
+            }
+        }
+
+        private static void ConvertChildren(List<LineItem> childItems, Dictionary<int, RowViewModel> dict)
+        {
+            var lastCount = childItems.Count + 1;
+            while (childItems.Count > 0 && childItems.Count < lastCount)
+            {
+                for (int i = 0; i < childItems.Count; i++)
+                {
+                    var item = childItems[i];
+                    // find parent 
+                    if (dict.ContainsKey(item.Parent.LineItemId))
+                    {
+                        var parentRow = dict[item.Parent.LineItemId];
+                        var newchild = new RowViewModel().CopyFrom(item);
+                        dict.Add(item.LineItemId, newchild);
+                        RowViewModel.LinkParentAndChild(parentRow, newchild);
+                        newchild.Indent = parentRow.Indent + 1;
+                        childItems.RemoveAt(i);
+                        i--;
+                    }
+                }
+                lastCount = childItems.Count;
+            }
+        }
+
+        private void ConvertParents(List<LineItem> parentItems, Dictionary<int, RowViewModel> dict)
+        {
+            HierarchicalRows = new List<RowViewModel>();
+            foreach (var item in parentItems)
+            {
+                var rowItem = new RowViewModel().CopyFrom(item);
+                HierarchicalRows.Add(rowItem);
+                dict.Add(rowItem.Id, rowItem);
+                rowItem.Indent = 0;
+            }
+        }
+
+        private static void SplitIntoParentVsChild(Project project, out List<LineItem> parentItems, out List<LineItem> childItems)
+        {
+            parentItems = new List<LineItem>();
+            childItems = new List<LineItem>();
+            foreach (var item in project.LineItems)
+            {
+                if (item.Parent == null)
+                {
+                    parentItems.Add(item);
+                }
+                else
+                {
+                    childItems.Add(item);
+                }
+            }
+        }
+
+        private void RecurseAppend(List<RowViewModel> HierarchicalRows, List<RowViewModel> AllRows)
+        {
+            foreach (var item in HierarchicalRows)
+            {
+                AllRows.Add(item);
+                if (item.Children.Count > 0)
+                {
+                    RecurseAppend(item.Children, AllRows);
+                }
+            }
+        }
+    }
+}
--- BD2012/Models/RowViewModel.cs
+++ BD2012/Models/RowViewModel.cs
@@ -1,158 +1,43 @@
-﻿
-
+﻿using bd2012.data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using bd2012.data;
-using System;
+using System.Text;
 
 namespace BD2012.Models
 {
-    public class BurndownViewModel
+    public class RowViewModel
     {
-        public BurndownViewModel()
+        public RowViewModel()
         {
-            AllRows = new List<RowItem>();
-            HierarchicalRows = new List<RowItem>();
-            Columns = new List<string>(); 
+            Children = new List<RowViewModel>();
+            Cells = new Dictionary<string, CellViewModel>(); 
         }
-
         public string Name { get; set; }
-        public List<RowItem> HierarchicalRows { get; private set; } 
-        public List<RowItem> AllRows { get; private set; }
-        public List<string> Columns { get; private set; } 
+        public int Id { get; set; }
+        public int Indent { get; set; }
+        public List<RowViewModel> Children { get; private set; }
+        public RowViewModel Parent { get; set; }
 
-        public BurndownViewModel CopyFrom(Project project)
+        public Dictionary<string, CellViewModel> Cells { get; set; }
+
+        public RowViewModel CopyFrom(LineItem item)
         {
-            Name = project.ProjectName;
+            Name = item.Name;
+            Id = item.LineItemId; 
 
-            List<LineItem> parentItems;
-            List<LineItem> childItems;
-            SplitIntoParentVsChild(project, out parentItems, out childItems);
-
-            Dictionary<int, RowItem> dict = new Dictionary<int, RowItem>();
-            ConvertParents(parentItems, dict);
-            ConvertChildren(childItems, dict);
-
-            AllRows = new List<RowItem>();
-            RecurseAppend(HierarchicalRows, AllRows);
-
-            MapData(project, dict);
-
-            foreach (var row in HierarchicalRows) 
-                UpdateCalculations(row);
-
-            return this; 
+            return this;
         }
 
-        private void UpdateCalculations(RowItem parentRow)
-        {
-            if (parentRow.Children != null && parentRow.Children.Count > 0)
-            {
-                foreach (var row in parentRow.Children) UpdateCalculations(row);
-
-                // i have children, therefore i am calculated
-                foreach (var c in Columns)
-                {
-                    decimal sum = 0m;
-                    foreach (var childRow in parentRow.Children)
-                    {
-                        decimal d;
-                        if (childRow.Data.TryGetValue(c, out d))
-                        {
-                            sum += d;
-                        }
-                    }
-                    parentRow.Data[c] = sum;
-                    parentRow.IsEditable = false;
-                }
-            }
-            else
-            {
-                parentRow.IsEditable = true;
-            }
+        public static void LinkParentAndChild(RowViewModel parent, RowViewModel child) {
+            parent.Children.Add(child);
+            child.Parent = parent; 
         }
+    }
 
-        private void MapData(Project project, Dictionary<int, RowItem> dict)
-        {
-            foreach (var dataCol in project.ColumnDefinitions)
-            {
-                Columns.Add(dataCol.ColumnName);
-            }
-
-            foreach (var d in project.Data.Where(x => x.Value.HasValue))
-            {
-                RowItem li;
-                if (dict.TryGetValue(d.LineItem.LineItemId, out li))
-                {
-                    if (li == null) continue;
-                    li.Data[d.Column.ColumnName] = d.Value.Value;
-                }
-            }
-        }
-
-        private static void ConvertChildren(List<LineItem> childItems, Dictionary<int, RowItem> dict)
-        {
-            var lastCount = childItems.Count + 1;
-            while (childItems.Count > 0 && childItems.Count < lastCount)
-            {
-                for (int i = 0; i < childItems.Count; i++)
-                {
-                    var item = childItems[i];
-                    // find parent 
-                    if (dict.ContainsKey(item.Parent.LineItemId))
-                    {
-                        var parentRow = dict[item.Parent.LineItemId];
-                        var newchild = new RowItem().CopyFrom(item);
-                        dict.Add(item.LineItemId, newchild);
-                        RowItem.LinkParentAndChild(parentRow, newchild);
-                        newchild.Indent = parentRow.Indent + 1;
-                        childItems.RemoveAt(i);
-                        i--;
-                    }
-                }
-                lastCount = childItems.Count;
-            }
-        }
-
-        private void ConvertParents(List<LineItem> parentItems, Dictionary<int, RowItem> dict)
-        {
-            HierarchicalRows = new List<RowItem>();
-            foreach (var item in parentItems)
-            {
-                var rowItem = new RowItem().CopyFrom(item);
-                HierarchicalRows.Add(rowItem);
-                dict.Add(rowItem.Id, rowItem);
-                rowItem.Indent = 0;
-            }
-        }
-
-        private static void SplitIntoParentVsChild(Project project, out List<LineItem> parentItems, out List<LineItem> childItems)
-        {
-            parentItems = new List<LineItem>();
-            childItems = new List<LineItem>();
-            foreach (var item in project.LineItems)
-            {
-                if (item.Parent == null)
-                {
-                    parentItems.Add(item);
-                }
-                else
-                {
-                    childItems.Add(item);
-                }
-            }
-        }
-
-        private void RecurseAppend(List<RowItem> HierarchicalRows, List<RowItem> AllRows)
-        {
-            foreach (var item in HierarchicalRows)
-            {
-                AllRows.Add(item);
-                if (item.Children.Count > 0)
-                {
-                    RecurseAppend(item.Children, AllRows);
-                }
-            }
-        }
+    public class CellViewModel
+    {
+        public decimal Value { get; set; }
+        public bool IsEditable { get; set; } 
     }
 }
